965c09367d8d44118dd631bcb96ebca0bf6b4751
mark-perdon <markanthonyperdon@gmail.com>
2019-03-05 12:51:08 +0800

    initialize

---
+++ simple_bs.py
@@ -0,0 +1,8 @@
+from bs4 import BeautifulSoup
+import requests
+url = input("Enter a website: ")
+r=requests.get("http://"+url)
+data=r.text
+soup=BeautifulSoup(data)
+for link in soup.find_all('a'):
+	print(link.get("href"))
